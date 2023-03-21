d3b8360d4188ca819d03d922c72ca21a9a575186
Lummiense <Chumichael@mail.ru>
2023-03-21 17:54:41 +0300

    Confir EfRepo and add DbContext

--- src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -1,37 +1,78 @@
-﻿using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
 {
-    internal class EfRepository <T> : IRepository<T>
+        public class EfRepository <T> : IRepository<T>
       where T : BaseEntity
     {
+        protected readonly DbContext _dbContext;
+        protected EfRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Получить из БД список всех сущностей
+        /// </summary>
+        /// <returns>Список сущностей</returns>
         public Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var result = _dbContext.Set<T>().ToList().AsEnumerable();
+            return Task.FromResult(result);
         }
 
+        /// <summary>
+        /// Получить сущность из БД по заданному id
+        /// </summary>
+        /// <param name="id">id интересующей сущности</param>
+        /// <returns>Интереующая сущность</returns>
         public Task<T> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
-        }
-        public Task AddAsync(T entity)
-        {
-            throw new NotImplementedException();
+            var result = _dbContext.Set<T>().FirstOrDefault(x=>x.Id==id);
+            return Task.FromResult(result);
         }
 
+        /// <summary>
+        /// Добавить новую сущность в БД
+        /// </summary>
+        /// <param name="entity">Сущность для добавления</param>
+        /// <returns>???</returns>
+        public Task AddAsync(T entity)
+        {
+            var result = _dbContext.Add(entity);
+            return Task.FromResult(result);
+        }
+
+        /// <summary>
+        /// Удалить сущность из БД
+        /// </summary>
+        /// <param name="id">id сущности, которую нужно удалить</param>
+        /// <returns>???</returns>
         public Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var entity = _dbContext.Set<T>().FirstOrDefault(x => x.Id == id);
+            var result = _dbContext.Remove(entity);
+            return Task.FromResult(result);
         }
-       
+
+        /// <summary>
+        /// Обновить данные сущности в БД
+        /// </summary>
+        /// <param name="entity">Обновленная сущность</param>
+        /// <returns>???</returns>
         public Task UpdateAsync(T entity)
         {
-            throw new NotImplementedException();
+            var result = _dbContext.Update(entity);
+            return Task.FromResult(result);
         }
     }
 }
--- src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
@@ -19,9 +22,12 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>((x =>
+                x.UseSqlite(Configuration.GetConnectionString("ConnectionString"))));
             services.AddScoped(typeof(IRepository<Employee>), (x) => 
                 new InMemoryRepository<Employee>(FakeDataFactory.Employees));
             services.AddScoped(typeof(IRepository<Role>), (x) => 
